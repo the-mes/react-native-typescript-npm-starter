30cb2864378c4550ca063c802d2529f81131b0f8
Jakub Biesiada <kbba@op.pl>
2021-08-16 21:43:06 +0200

    chore: update

--- __tests__/HelloWorld.test.tsx
+++ __tests__/HelloWorld.test.tsx
@@ -1,6 +1,22 @@
+import React from 'react'
+import { render } from '@testing-library/react-native'
+
+import { HelloWorld } from '../src'
+
+// TODO define platform
+
 describe('HelloWorld', () => {
-    it('', () => {
-      // TODO
-    })
+  it('should render component with default message', () => {
+    const { getByText, debug } = render(<HelloWorld />)
+
+    debug()
+
+    expect(getByText('Hello')).toBeDefined()
   })
-  
+
+  it('should render component with custom message', () => {
+    const { getByText } = render(<HelloWorld text="Good Morning!" />)
+
+    expect(getByText('Good Morning!')).toBeDefined()
+  })
+})
